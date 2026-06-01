2c85effe7261719b0e7f185cdc4ea004618584c7
Masibulele <masibulele100@gmail.com>
2026-06-01 18:53:57 +0200

    dynamically render individual post

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("",views.index,name="index"),
-    path("post/<int:post_id>",views.post_details, name="post"),
+    path("post/<int:post_id>",views.post_details, name="post_details"),
 
 ]
 
